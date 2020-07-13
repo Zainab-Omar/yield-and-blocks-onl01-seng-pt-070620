4c8c5af976636b04c583b586c9ab38adcbdc3468
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-13 19:35:34 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,16 @@
 def hello_t(array)
 i=0
 while i<array.length
+yield array[i]
 i+=1
 end
 end
 
+names=["Tim","Tom","Jim"]
+hello_t(names) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{nmae}"
+  end
+end
 # call your method here!
 

fb381441ab24eea3204a03a810851ca2ccd5132b
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-13 19:35:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,10 +7,11 @@ end
 end
 
 names=["Tim","Tom","Jim"]
-hello_t(names) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{nmae}"
-  end
-end
+
 # call your method here!
 
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
